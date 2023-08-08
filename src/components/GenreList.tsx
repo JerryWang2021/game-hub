87b34b17e998ed5d599dcc48d1af70a4b83cb8b0
Zhi Wang <jerrywang65@hotmail.com>
2023-08-07 21:33:24 -0700

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -24,15 +25,21 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
   return (
     <List>
+      <Heading fontSize={"2xl"} marginBottom={3}>
+        Genres
+      </Heading>
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image
               boxSize="32px"
               borderRadius={8}
+              objectFit={"cover"}
               src={getCroppedImageUrl(genre.image_background)}
             />
             <Button
+              whiteSpace={"normal"}
+              textAlign={"left"}
               fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
